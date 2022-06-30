99d4cde73614b3fe26d484472b74941fd2c2309d
anil843324 <anilkrsingh14200@gmail.com>
2022-06-30 22:57:43 +0530

    now completed

--- StopWatch/index.js
+++ StopWatch/index.js
@@ -7,63 +7,47 @@ const start_btn = document.getElementById('start');
 const stop_btn = document.getElementById('stop');
 const reset_btn = document.getElementById('reset');
 
-
-let second = 0;
+let second = 3670;
 
 let inteval = null;
 
-
 //  Event listners
 
-start_btn.addEventListener('click',start)
-stop_btn.addEventListener('click',stop)
-reset_btn.addEventListener('click',reset)
+start_btn.addEventListener('click', start)
+stop_btn.addEventListener('click', stop)
+reset_btn.addEventListener('click', reset)
 
 //   Update the time
 
-
 const timer = () => {
-
-
     second++;
     //   Format our time
     let hour = Math.floor(second / 3600);
     let min = Math.floor((second - (hour * 3600)) / 60);
     let secs = (second % 60);
 
-    
-
     if (secs < 10) secs = '0' + secs;
     if (min < 10) min = '0' + min;
     if (hour < 10) hour = '0' + hour;
 
     time_el.innerText = `${hour}:${min}:${secs}`;
-
-
 }
 
 
-  function start(){
-    if(inteval){
-        return
-    }
-    inteval=setInterval(timer,1000)
-    
+function start() {
+   
+    inteval = setInterval(timer, 1000)
+   
+}
 
- }
-
- function stop(){
-  
+function stop() {
     clearInterval(inteval)
-     inteval=null;
+   
 }
-function reset(){
-
-      stop();
-      time_el.innerText='00:00:00'
-      second=0;
-
-
+function reset() {
+    stop();
+    time_el.innerText = '00:00:00'
+    second = 0;
 }
 
 
