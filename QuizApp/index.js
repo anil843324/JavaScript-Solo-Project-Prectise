b6a1812b55d0a18618913c8330703661d5d02c6b
anil843324 <anilkrsingh14200@gmail.com>
2022-07-03 20:59:23 +0530

    ui part done

---
+++ QuizApp/index.js
@@ -0,0 +1,59 @@
+
+const quizData = [
+
+    {
+        question: "How old is Florin?",
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+
+    },
+    {
+        question: "What is the most used programming language in 2019",
+        a: 'Java',
+        b: 'c',
+        c: 'Python',
+        d: 'javaScript',
+        correct: 'd'
+    },
+    {
+        question: "Who is he president of US?",
+        a: 'Florian Pop',
+        b: 'Donland Trump',
+        c: 'Invan Saldano',
+        d: 'Joe biden',
+        correct: 'd'
+
+    },
+    {
+        question: "What does HTML stand for ?",
+        a: 'Hypertext Markup Language',
+        b: 'Coascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicoper Terminals Motobats Lamborigins',
+        correct: 'a'
+
+    },
+    {
+        question: "What is year was JavaScript lanuched ?",
+        a: '2020',
+        b: '2019',
+        c: '2000',
+        d: 'none of the above',
+        correct: 'd'
+    },
+    {
+        question: "What is year was JavaScript lanuched ?",
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'b'
+    },
+
+
+
+
+]
